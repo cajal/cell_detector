e8fe298918db77a24ce622f7ec6fbfd990b2b9ab
Fabian Sinz <sinz@bcm.edu>
2016-01-13 12:41:12 -0600

    add cell location correction

--- predict_stack.py
+++ predict_stack.py
@@ -16,13 +16,16 @@ def parse_command_line():
     parser.add_argument('stackfile', help='hdf5 file containing the 250x250x60 stack as "stack"')
     parser.add_argument('outfile', help='Output file hdf5 file. The probability is stored as "P" the cells as "cells"')
     parser.add_argument('--sites', type=int, help='number of sites to find (default=10)', default=10)
+    parser.add_argument('--correct', type=int,
+                        help='''correct cell locations in a radius of n around the old
+                                locations supplied as "cells" in the hdf5 file''')
     parser.add_argument('--plot', type=str, help='plot detected cells to this path')
 
     return parser.parse_args()
 
 
 def max_iterator(P, n, voxel):
-    P = np.asarray(P)
+    P = np.array(P) # copy
     i, j, k = [i // 2 for i in voxel]
     counter = 0
 
@@ -32,6 +35,17 @@ def max_iterator(P, n, voxel):
         P[x - i:x + i, y - j:y + j, z - k:z + k] = -1
         yield (x, y, z)
         counter += 1
+
+def correction_iterator(P, old_locations, radius, voxel):
+
+    offset = np.asarray([i // 2 for i in voxel], dtype=int)
+    for loc in old_locations:
+        fro = np.asarray([max(0, e) for e in loc - radius - offset])
+        to = np.asarray([min(b, e) for b, e in zip(P.shape, loc + radius + offset)])
+        p = P[tuple(slice(f,t) for f,t in zip(fro, to))]
+        locs = np.mgrid[tuple(slice(f,t) for f,t in zip(fro, to))]
+        yield locs[:, p == p.max()]
+
 
 
 if __name__ == '__main__':
@@ -48,15 +62,28 @@ if __name__ == '__main__':
 
     with h5py.File(args.stackfile) as fid:
         X = preprocessor(np.asarray(fid['stack'], dtype=float).squeeze())
+        # subtract 1 for matlab python index compatibility
+        old_cells = np.asarray(fid['cells'], dtype=int) - 1 if 'cells' in fid else None
 
-    P = model.P(X, full=True)
 
-    cells = list(max_iterator(P, args.sites, voxel))
+    P = model.P(X, full=True) # get probability map
+
+    with h5py.File(args.outfile) as fid:
+        if args.correct:
+            assert old_cells is not None, "You must specify cell locations in 'cells' for correction"
+            new_cells = list(correction_iterator(P, old_cells, args.correct, voxel))
+            new_cells = np.asarray(new_cells, dtype=int)
+            fid.create_dataset('corrected_cells', new_cells.shape, dtype=int, data=new_cells + 1)
+        else:
+            new_cells = list(max_iterator(P, args.sites, voxel))
+            new_cells = np.asarray(new_cells, dtype=int)
+            fid.create_dataset('cells', new_cells.shape, dtype=int, data=new_cells + 1)
+            fid.create_dataset('P', P.shape, dtype=float, data=P)
 
     if args.plot:
         sns.set_style('white')
 
-        for i, (x, y, z) in enumerate(cells):
+        for i, (x, y, z) in enumerate(new_cells):
             fig, ax = plt.subplots()
             ax.imshow(X[..., z], cmap=plt.cm.gray, interpolation='nearest')
             ax.plot(y, x, 'ok', mfc='orange')
@@ -64,11 +91,3 @@ if __name__ == '__main__':
             ax.axis('tight')
             fig.savefig('%s/cell%03i.png' % (args.plot, i))
             plt.close(fig)
-
-    with h5py.File(args.outfile,'w') as fid:
-        cells = np.asarray(cells, dtype=int)
-        fid.create_dataset('cells', cells.shape, dtype=int, data=cells+1)
-        fid.create_dataset('P', P.shape, dtype=float, data=P)
-
-
-
